8bf75e5766b4738c6ce0f4c37516bbba00f4574a
Gustavo Almeida <gustavo_80b@hotmail.com>
2023-01-10 11:36:37 -0300

    Ajustes QR Code Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 // Pages
 import Home from './pages/Home/Home';
-import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <Container fluid className="bg-dark py-5">
+    <Container fluid className="bg-dark py-5 min-vh-100">
       <Container>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </Container>
     </Container>
